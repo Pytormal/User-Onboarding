edd6c96c164ab3b31df7ef746a359808c0304a48
Alex Andrew <pytormal@live.com>
2020-09-10 17:38:02 -0700

    formatted to be centered.

--- form-managment/src/components/Form.js
+++ form-managment/src/components/Form.js
@@ -4,10 +4,10 @@ import axios from "axios";
 
 const formSchema = yup.object().shape({
   name: yup.string().required("Name required"),
-  email: yup.string().email("email needed").required("must include email"),
+  email: yup.string("@").email("email needed").required("must include email"),
   password: yup
     .string()
-    .min(6, "passwrod needs to be more than 6 characters long")
+    .min(6, "password needs to be more than 6 characters long")
     .required("password needed"),
   terms: yup.boolean().oneOf([true], "please validate"),
 });
@@ -21,7 +21,7 @@ export default function Form() {
   const [buttonDisabled, setButtonDisabled] = useState(true);
   useEffect(() => {
     formSchema.isValid(formState).then((valid) => {
-      setButtonDisabled(!valid);
+      setButtonDisabled(valid);
     });
   }, [formState]);
 
@@ -71,56 +71,70 @@ export default function Form() {
 
   return (
     <form onSubmit={formSubmit}>
-      <label htmlFor="name">
-        <input
-          id="name"
-          type="text"
-          name="name"
-          placeholder="Name"
-          value={formState.name}
-          onChange={inputChange}
-        />
-      </label>
-      <label htmlFor="email">
-        <input
-          id="email"
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={formState.email}
-          onChange={inputChange}
-        />
-        {errState.email.length < 0 ? (
-          <p ClassName="error">{errState.email}</p>
-        ) : null}
-      </label>
-      <label htmlFor="password">
-        <input
-          id="password"
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={formState.password}
-          onChange={inputChange}
-        />
-        {errState.password.length > 6 ? (
-          <p ClassName="error">{errState.password}</p>
-        ) : null}
-      </label>
-      <label htmlFor="validate">
-        <input
-          type="checkbox"
-          id="terms"
-          name="terms"
-          checked={formState.terms}
-          onChange={inputChange}
-        />
-        Validate Here
-        {errState.terms.length > 0 ? (
-          <p className="error">{errState.terms}</p>
-        ) : null}
-      </label>
-      <button disabled={buttonDisabled}>Submit</button>
+      <ul>
+        <label htmlFor="name">
+          <div>
+            <input
+              id="name"
+              type="text"
+              name="name"
+              placeholder="Name"
+              value={formState.name}
+              onChange={inputChange}
+            />
+          </div>
+        </label>
+        <label htmlFor="email">
+          <div>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={formState.email}
+              onChange={inputChange}
+            />
+          </div>
+
+          {errState.email.length < 0 ? (
+            <p ClassName="error">{errState.email}</p>
+          ) : null}
+        </label>
+        <label htmlFor="password">
+          <div>
+            {" "}
+            <input
+              id="password"
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={formState.password}
+              onChange={inputChange}
+            />
+          </div>
+
+          {errState.password.length > 6 ? (
+            <p ClassName="error">{errState.password}</p>
+          ) : null}
+        </label>
+        <label htmlFor="validate">
+          <div>
+            Validate Here
+            <input
+              type="checkbox"
+              id="terms"
+              name="terms"
+              checked={formState.terms}
+              onChange={inputChange}
+            />
+          </div>
+
+          {errState.terms.length > 0 ? (
+            <p className="error">{errState.terms}</p>
+          ) : null}
+        </label>
+        <button disabled={!buttonDisabled}>Submit</button>
+      </ul>
     </form>
   );
 }
